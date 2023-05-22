8521ec0e29b2f5dbd64a00fb9789aedb602220a6
juan bernal <mesieou@gmail.com>
2023-05-22 17:11:50 +0900

    changes

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,3 +1,18 @@
 class MeetingsController < ApplicationController
+  def show
+    @meeting = Meeting.find(params[:id])
+  end
 
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    if @meeting.save
+      redirect_to meetings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
