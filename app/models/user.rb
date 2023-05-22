58a111435ad63103517e9a267f2f2657c8d229da
Xuemei Huang <mei.huang9500@gmail.com>
2023-05-22 17:11:16 +0900

    added index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   has_many :meetings, through: :bookings
   has_one_attached :photo
 
-  validates :name, presence: true
-  validates :job_title, presence: true
-  validates :mobile, presence: true
-  validates :role, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  # validates :name, presence: true
+  # validates :job_title, presence: true
+  # validates :mobile, presence: true
+  # validates :role, presence: true
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
