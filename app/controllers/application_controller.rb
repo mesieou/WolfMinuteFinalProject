b19104a8a0ff61320732c303d1305923f0c9c71c
Xuemei Huang <mei.huang9500@gmail.com>
2023-05-23 14:16:57 +0900

    add cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters
   before_action :authenticate_user!
   include Pundit::Authorization
 
@@ -17,5 +19,12 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :photo) }
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :photo) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :name, :role, :mobile, :job_title])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :name, :role, :mobile, :job_title])
   end
 end
