0c803aa130a13ae10febfb344b8c9260df61271e
Jun Ukemori <jukemori@gmail.com>
2023-05-23 12:51:27 +0900

    calendar added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'date'
 puts "cleaning users, meetings and bookings..."
 Booking.destroy_all
 Meeting.destroy_all
@@ -13,7 +14,11 @@ User.destroy_all
 
 role = ["manager", "employee"]
 location = ["room 1", "room 2", "room 3", "room 4", "room 5"]
-creator = [true, false]
+duration = [15, 20, 30, 60]
+# random_date = rand(1..29)
+# random_hour = rand(9..17)
+# random_minute = [15, 30, 60].sample
+now = DateTime.now
 
 test_user = User.create!(
   name: "testuser",
@@ -29,12 +34,14 @@ puts "created test user"
 test_meeting = Meeting.create!(
   title: Faker::Company.buzzword,
   user: test_user,
-  start_date: Date.today + rand(1..3),
+  start_date: DateTime.new(now.year, now.month, rand(1..29), rand(9..17), [15, 30, 60].sample, 0),
   end_date: Date.today + rand(4..6),
   description: Faker::Company.catch_phrase,
   location: location.sample,
-  duration: rand(1..3)
+  duration: duration.sample
 )
+
+p test_meeting.start_date
 
 puts "created meating"
 
@@ -56,15 +63,18 @@ puts "created booking"
   )
   puts "created #{User.count} users!"
 
+20.times do
   meeting = Meeting.create!(
     title: Faker::Company.buzzword,
     user: user,
-    start_date: Date.today + rand(1..3),
+    start_date: DateTime.new(now.year, now.month, rand(1..29), rand(9..17), [15, 30, 60].sample, 0),
     end_date: Date.today + rand(4..6),
     description: Faker::Company.catch_phrase,
     location: location.sample,
-    duration: rand(1..3)
+    duration: duration.sample
   )
+  p meeting.start_date
+end
 
   puts "created #{Meeting.count} meetings!"
 end
