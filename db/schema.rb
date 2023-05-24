7a8120e174f115ec2ceb6d056158146b0a6aaba1
Xuemei Huang <mei.huang9500@gmail.com>
2023-05-24 22:51:14 +0900

    added chatroom message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_031836) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_134823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_031836) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "meetings", force: :cascade do |t|
     t.datetime "start_date"
     t.string "description"
@@ -67,6 +73,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_031836) do
     t.index ["video_id"], name: "index_meetings_on_video_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -80,6 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_031836) do
     t.string "job_title"
     t.string "mobile"
     t.string "role"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -100,4 +117,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_031836) do
   add_foreign_key "bookings", "users"
   add_foreign_key "meetings", "users"
   add_foreign_key "meetings", "videos"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
