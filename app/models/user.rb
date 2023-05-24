de866d724796fa2d92b6d6abd8529d95e6c78f18
exec zsh <wind10cloud@gmail.com>
2023-05-24 12:36:18 +0900

    created find available mathod

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,46 +14,34 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-#  def available?(year, month, date, hour)
-#   start_date = Time.local(year, month, date, hour).in_time_zone('Tokyo')
-#   self.bookings.any? do |booking|
-#     p start_date
-#     p booking.meeting.start_date.in_time_zone('Tokyo')
-#     !(booking.meeting.start_date.in_time_zone('Tokyo') - start_date < 0.003)
-#   end
-#  end
-
  def available_time(year, month, day)
-  # date = DateTime.now.in_time_zone('Tokyo')
   date = Time.local(year, month, day)
-  # date = Time.local(2023, 5, 30, 11, 0)
   today_meetings = self.meetings.where(start_date: date.beginning_of_day..date.end_of_day)
-  # today_bookings = self.bookings.select { |booking| booking.meeting.start_date.day == date.day }
   time_slots = (9..18).to_a
   hash = {}
   if today_meetings.count >= 1
   time_slots.each do |hour|
-    # available = today_meetings.any? do |meeting|
-
-      # booking.meeting.start_date.in_time_zone('Tokyo').hour != hour && booking.meeting.end_date.in_time_zone('Tokyo').hour != hour || booking.meeting.start_date.in_time_zone('Tokyo').hour < hour && booking.meeting.end_date.in_time_zone('Tokyo').hour <= hour
-
-
-      # available = today_meetings.find_by("DATE_PART('hour', start_date) = ?", hour) ? false : true
-
-    # end
     hash[hour] = true
-    today_meetings.each do |meeting|
-    (meeting.start_date.hour...meeting.start_date.hour + meeting.duration.to_i).to_a.each do |hour|
-      hash[hour] = false
+    today_meetings.each { |meeting| (meeting.start_date.hour...meeting.start_date.hour + meeting.duration.to_i).to_a.each { |hour| hash[hour] = false } }
     end
-  end
-  end
   hash
-else
-  time_slots.each do |hour|
-    hash[hour] = true
+  else
+  time_slots.each { |hour| hash[hour] = true }
   end
+  hash.select { |key, value| value == true }.keys
 end
-hash
+
+def self.find_available(users)
+    # users should be the array of attendees
+    date = Date.tomorrow
+    count = users.count
+    array = users.map { |user| user.available_time(date.year, date.month, date.day) }.flatten.sort
+    ava = array.select { |value| array.count(value) >= count }.uniq
+    while ava.count.zero?
+      date = Date.tomorrow + 1
+      array = users.map { |user| user.available_time(date.year, date.month, date.day) }.flatten.sort
+      ava = array.select { |value| array.count(value) >= count }.uniq
+    end
+    "Next available time is on #{date.day}/#{date.month} at #{ava.min}:00."
 end
 end
