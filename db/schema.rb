e824c4661b53b2695a85d1a1c3df609299f4bc92
juan bernal <mesieou@gmail.com>
2023-05-25 22:53:09 +0900

    same as before

--- db/schema.rb
+++ db/schema.rb
@@ -90,8 +90,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_021053) do
     t.string "job_title"
     t.string "mobile"
     t.string "role"
-    t.boolean "admin", default: false
     t.string "nickname"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
