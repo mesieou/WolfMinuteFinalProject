5df5a4fca486296b0d660c3b4be1ce53892f0e17
exec zsh <wind10cloud@gmail.com>
2023-05-25 18:27:48 +0900

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_062146) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_021053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,8 +90,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_062146) do
     t.string "job_title"
     t.string "mobile"
     t.string "role"
-    t.string "nickname"
     t.boolean "admin", default: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
