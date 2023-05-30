3e8c820dee5a5fab935f4a37b6dab9bc3c9aea3f
Jun Ukemori <jukemori@gmail.com>
2023-05-30 14:20:08 +0900

    timebar

---
+++ app/javascript/controllers/progress_bar_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="progress-bar"
+export default class extends Controller {
+  static targets = ["progress"]
+  static values = {
+    duration: Number
+  }
+
+  connect() {
+    console.log(this.durationValue); // converted to milliseconds
+    this.countdown()
+  }
+
+  countdown() {
+    const startTime = Date.now();
+    setInterval(() => {
+      const progressEl = document.getElementById("progress")
+      const elapsedTime = Date.now() - startTime;
+      let progress = (elapsedTime / 60000) * 100
+      // console.log(progress, startTime, elapsedTime);
+      progressEl.style.width = `${progress}%`;
+    }, 1000);
+
+  }
+
+}
