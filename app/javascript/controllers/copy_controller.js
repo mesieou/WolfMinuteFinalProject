649ad21d78957fd27647e1b3b9d74e58f8a0c828
Xuemei Huang <mei.huang9500@gmail.com>
2023-06-01 17:31:18 +0900

    add icon for the button

--- app/javascript/controllers/copy_controller.js
+++ app/javascript/controllers/copy_controller.js
@@ -7,12 +7,13 @@ static targets = ["text"]
   connect() {
   }
 
-  copyToClipboard() {
+  copyToClipboard(event) {
     let text = ""
     this.textTargets.forEach((p) => {
       text = text + p.textContent + " "
 
     })
+    event.currentTarget.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
 
     navigator.clipboard.writeText(text)
   }
